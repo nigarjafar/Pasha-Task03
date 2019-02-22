c3b3bca136fc3354a5cf63377dab78228b44f723
Nigar Jafar <nigar.c@code.edu.az>
2019-02-22 15:34:51 +0400

    Counting dispatches

--- src/reducers/countReducer.js
+++ src/reducers/countReducer.js
@@ -1,8 +1,3 @@
-import { POSTS_LOADED, USERS_LOADED } from "../actions/types";
-
 export default (state = 0, action) => {
-	if (action.type == POSTS_LOADED || action.type == USERS_LOADED)
-		return ++state;
-
-	return state;
+	return ++state;
 };
